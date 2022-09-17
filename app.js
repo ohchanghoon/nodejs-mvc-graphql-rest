c8905905133a004da42352bdebe7ab9b05a2233b
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-09-18 00:11:43 +0900

    add: add middleware

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require('http');
 
-const server = http.createServer(routes);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // Alows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log('In anthor middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
