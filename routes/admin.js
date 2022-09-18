7918292b46a6fca564cafa55ecee9525205673be
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-09-18 23:18:54 +0900

    add: add error router 404

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/app-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   res.send(
     `<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`
   );
