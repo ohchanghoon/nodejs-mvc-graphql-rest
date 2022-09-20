415f77c837d280a5b1895b324af0e4c64b723171
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-09-20 17:23:38 +0900

    add: stying page

--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,7 @@ const router = express.Router();
 
 /** /admin/add-product => GET */
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'app-product.html'));
-  // res.sendFile(path.join(__dirname, '..', 'views', 'app-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 /** /admin/add-product => POST */
